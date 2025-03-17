fix rnn train to update gradient every batch, not every epoch

training/state_dependent_model_training.py
@@ -4,69 +4,63 @@ from torch.utils.data import DataLoader
 from models import StateDependentModule
 
 
-def is_zero(x: float, eps: float = 1e-12) -> bool:
-    if x < eps:
-        return True
-    else:
-        return False
-
-def optim_update_step(optimizer, losses, history_losses, tokens_n):
-    running_train_loss = 0.0
+def optim_update_step(optimizer, losses, tokens_n):
     optimizer.zero_grad()
     for loss in losses:
         (loss.sum() / tokens_n).backward(retain_graph=True)
-        running_train_loss += loss.sum().item() / tokens_n
     optimizer.step()
 
-    history_losses.append(running_train_loss)
 
-
-def custom_update_step(model, losses, history_losses, tokens_n, lr):
-    running_train_loss = 0.0
+def custom_update_step(model, losses, tokens_n, lr):
     for param in model.parameters:
         param.grad = None
 
     for loss in losses:
         (loss.sum() / tokens_n).backward(retain_graph=True)
-        running_train_loss += loss.sum().item() / tokens_n
 
     for param in model.parameters:
         param.data -= param.grad * lr
 
-    history_losses.append(running_train_loss)
 
-
-def train_loop(model: StateDependentModule, train_loader: DataLoader, tokens_n: int, train_losses: list[float], lr=1e-3, optimizer=None) -> None:
+def train_loop(model: StateDependentModule, train_loader: DataLoader, train_losses: list[float], lr=1e-3, optimizer=None) -> None:
     hidden_units, number_of_hidden_layers = model.hidden_states_dimensions()
     model.set_train_mode()
 
-    losses = []
+    running_train_loss = 0.0
+    all_tokens = 0
     for x_seq, y_seq, w_seq in train_loader:
+        losses = []
+        tokens = w_seq.sum().sum().item()
+        all_tokens += tokens
         hidden_states = torch.zeros(len(x_seq), hidden_units, number_of_hidden_layers)
         for i in range(x_seq.shape[1]):
             preds, hidden_states = model(x_seq[:, i], hidden_states)
             losses.append(F.cross_entropy(preds, y_seq[:, i], reduction='none') * w_seq[:, i])
+            running_train_loss += losses[-1].sum().item()
 
-    if optimizer is None:
-        custom_update_step(model, losses, train_losses, tokens_n, lr)
-    else:
-        optim_update_step(optimizer, losses, train_losses, tokens_n)
+        if optimizer is None:
+            custom_update_step(model, losses, tokens, lr)
+        else:
+            optim_update_step(optimizer, losses, tokens)
+
+    train_losses.append(running_train_loss / all_tokens)
 
 
-
-def val_loop(model: StateDependentModule, val_loader: DataLoader, tokens_n: int, val_losses: list[float]) -> None:
+def val_loop(model: StateDependentModule, val_loader: DataLoader, val_losses: list[float]) -> None:
     running_val_loss = 0.0
     hidden_units, number_of_hidden_layers = model.hidden_states_dimensions()
     model.set_eval_mode()
-
+    all_tokens = 0
     for x_seq, y_seq, w_seq in val_loader:
+        tokens = w_seq.sum().sum().item()
+        all_tokens += tokens
         hidden_states = torch.zeros(len(x_seq), hidden_units, number_of_hidden_layers)
         for i in range(x_seq.shape[1]):
             preds, hidden_states = model(x_seq[:, i], hidden_states)
             loss = F.cross_entropy(preds, y_seq[:, i], reduction='none') * w_seq[:, i]
-            running_val_loss += loss.sum().item() / tokens_n
+            running_val_loss += loss.sum().item()
 
-    val_losses.append(running_val_loss)
+    val_losses.append(running_val_loss / all_tokens)
 
 
 def print_stats(epoch: int, num_epochs: int, train_losses: list[float], val_losses: list[float]) -> None:
@@ -92,11 +86,11 @@ def train_model(model: StateDependentModule,
     for epoch in range(num_epochs):
         lr = 0.5
         if optimizer is not None:
-            train_loop(model, train_loader, train_tokens_n, train_losses, optimizer=optimizer)
+            train_loop(model, train_loader, train_losses, optimizer=optimizer)
         else:
-            train_loop(model, train_loader, train_tokens_n, train_losses, lr=lr)
+            train_loop(model, train_loader, train_losses, lr=lr)
 
-        val_loop(model, val_loader, val_tokens_n, val_losses)
+        val_loop(model, val_loader, val_losses)
         print_stats(epoch, num_epochs, train_losses, val_losses)
 
     return {